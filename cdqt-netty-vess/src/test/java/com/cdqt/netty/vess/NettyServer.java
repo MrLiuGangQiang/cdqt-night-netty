5e73ca2e4998cfcb3a66c06a6126252325878b36
LiuGangQiang <LiuGangQiang@192.168.10.119>
2020-12-16 18:05:37 +0800

    修改配置

--- cdqt-netty-vess/src/test/java/com/cdqt/netty/vess/NettyServer.java
+++ cdqt-netty-vess/src/test/java/com/cdqt/netty/vess/NettyServer.java
@@ -1,19 +1,25 @@
 package com.cdqt.netty.vess;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.cdqt.netty.vess.handler.http.FistHttpServerHandler;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class NettyServer {
+	private final static Logger LOGGER = LoggerFactory.getLogger(NettyServer.class);
 
 	private void bind(String host, int port) {
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
@@ -21,18 +27,22 @@ public class NettyServer {
 		ServerBootstrap bootstrap = new ServerBootstrap();
 		bootstrap.group(bossGroup, workerGroup);
 		bootstrap.channel(NioServerSocketChannel.class);
+		bootstrap.option(ChannelOption.SO_BACKLOG, 512);
+		bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
 		bootstrap.childHandler(new ChannelInitializer<Channel>() {
 
 			@Override
 			protected void initChannel(Channel ch) throws Exception {
 				ChannelPipeline pipeline = ch.pipeline();
-				pipeline.addLast(new HttpResponseEncoder());
-				pipeline.addLast(new HttpRequestDecoder());
+				pipeline.addLast(new HttpServerCodec());
+				pipeline.addLast(new ChunkedWriteHandler());
+				pipeline.addLast(new HttpObjectAggregator(Integer.MAX_VALUE));
 				pipeline.addLast(new FistHttpServerHandler());
 			}
 		});
 		try {
 			ChannelFuture future = bootstrap.bind(host, port).sync();
+			LOGGER.info("服务端启动成功，监听地址[{}:{}]", host, port);
 			future.channel().closeFuture().sync();
 		} catch (Exception e) {
 			e.printStackTrace();
